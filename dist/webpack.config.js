d10c86bfc727c4b0cae0e8c0b69ae1fe431156e4
Yash Arora <arorayash.thisisus@gmail.com>
2017-01-28 10:07:43 +0530

    Added webpack

---
+++ dist/webpack.config.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = {
+    context: __dirname + "/src",
+
+    entry: "./scripts/app.js",
+
+    output: {
+        filename: "app.js",
+        path: __dirname + "/dist"
+    }
+};
+//# sourceMappingURL=webpack.config.js.map
