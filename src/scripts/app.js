cb901a7e55c1cd8ff463f9b7aeeae225cf05b21a
Yash Arora <arorayash.thisisus@gmail.com>
2017-01-31 23:37:08 +0530

    added react-router and redux

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, Route, Link, browserHistory } from 'react-router';
 
 import Main from "./components/Main";
+import About from "./components/About";
+import Error404 from "./components/Error404";
 
 const appMount = document.getElementById("app");
 
-ReactDOM.render(<Main/>, appMount);
+const routes = <Router history = { browserHistory }>
+                <Route path="/" component={Main}/>
+                <Route path="about" component={About}/>
+                <Route path="*" component={Error404}/>
+             </Router>
+
+ReactDOM.render(routes, appMount);
